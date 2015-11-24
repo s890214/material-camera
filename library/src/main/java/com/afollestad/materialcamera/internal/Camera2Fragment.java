bfdc49eca68720ddd3386891cc8d7df1879e4e68
Aidan Follestad <drummer.aidan@gmail.com>
2015-11-24 13:09:36 -0600

    Camera2 CameraDevice error handling

--- library/src/main/java/com/afollestad/materialcamera/internal/Camera2Fragment.java
+++ library/src/main/java/com/afollestad/materialcamera/internal/Camera2Fragment.java
@@ -114,10 +114,26 @@ public class Camera2Fragment extends BaseCameraFragment implements View.OnClickL
             mCameraOpenCloseLock.release();
             cameraDevice.close();
             mCameraDevice = null;
-            Activity activity = getActivity();
-            if (null != activity) {
-                activity.finish();
+
+            String errorMsg = "Unknown camera error";
+            switch (error) {
+                case CameraDevice.StateCallback.ERROR_CAMERA_IN_USE:
+                    errorMsg = "Camera is already in use.";
+                    break;
+                case CameraDevice.StateCallback.ERROR_MAX_CAMERAS_IN_USE:
+                    errorMsg = "Max number of cameras are open, close previous cameras first.";
+                    break;
+                case CameraDevice.StateCallback.ERROR_CAMERA_DISABLED:
+                    errorMsg = "Camera is disabled, e.g. due to device policies.";
+                    break;
+                case CameraDevice.StateCallback.ERROR_CAMERA_DEVICE:
+                    errorMsg = "Camera device has encountered a fatal error, please try again.";
+                    break;
+                case CameraDevice.StateCallback.ERROR_CAMERA_SERVICE:
+                    errorMsg = "Camera service has encountered a fatal error, please try again.";
+                    break;
             }
+            throwError(new Exception(errorMsg));
         }
     };
 
